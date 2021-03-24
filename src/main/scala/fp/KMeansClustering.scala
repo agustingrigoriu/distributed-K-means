a6699f6ffc9278a294428b1194338e9546f857db
Agustin Gregorieu <agustin.gregorieu@gmail.com>
2021-03-24 15:48:50 -0400

    Adding cosine similarity.

--- src/main/scala/fp/KMeansClustering.scala
+++ src/main/scala/fp/KMeansClustering.scala
@@ -9,13 +9,31 @@ import org.apache.spark.sql.types.{LongType, StringType, StructType}
 object KMeansClusteringMain {
 
 
-  def parseToBOW(text: String): Array[(String, Int)] = {
+  def parseToBOW(text: String): Map[String, Int] = {
     val splitText = text.split("\\s")
     val wordsCount = splitText.map(word => (word, 1))
-    val groupedAndReducedCounts = wordsCount.groupBy(_._1).mapValues(_.map(_._2).sum)
-    val bow = groupedAndReducedCounts.toArray
+    val bow = wordsCount.groupBy(_._1).mapValues(_.map(_._2).sum)
 
     bow
+  }
+
+  def calculateCosineSimilarity(m1: Map[String, Int], m2: Map[String, Int]): Double = {
+
+    var dotProduct: Double = 0.0
+
+    m1.keys.foreach(word => {
+      if (m2.contains(word)) dotProduct += (m1.getOrElse(word, 0.0) + m2.getOrElse(word, 0.0))
+    })
+
+    val m1SumOfSquares = m1.values.map(freq => math.pow(freq, 2)).sum
+    val m2SumOfSquares = m2.values.map(freq => math.pow(freq, 2)).sum
+
+    val m1Norm = math.sqrt(m1SumOfSquares)
+    val m2Norm = math.sqrt(m2SumOfSquares)
+
+    val cosineSimilarity = dotProduct / (m1Norm * m2Norm)
+
+    cosineSimilarity
   }
 
   def main(args: Array[String]) {
@@ -34,37 +52,39 @@ object KMeansClusteringMain {
     val sc = spark.sparkContext
 
     val inputDir: String = args(0)
-//    val outputDir: String = args(2)
-//    val k: Int = args(0).toInt;
-//    val iterationsNumber: Int = args(1).toInt;
+    //    val outputDir: String = args(2)
+    //    val k: Int = args(0).toInt;
+    //    val iterationsNumber: Int = args(1).toInt;
 
 
     val schema = new StructType()
       .add("created_utc", LongType, nullable = false)
-      .add("title",StringType,true)
+      .add("title", StringType, true)
 
     //read all files from a folder
     val df = spark.read.schema(schema).json(inputDir)
     df.show(true)
 
     val bagOfWords = df.rdd
-      .filter( row => !row.isNullAt(1))
+      .filter(row => !row.isNullAt(1))
       .map(row => (row.getLong(0), row.getString(1)))
       .mapValues(title => parseToBOW(title))
 
-//    .filter(x => x.Document)
-//    .map( x => x.Document.replace(',',' ').replace('.',' ').replace('-',' ').lower())\
-//      .flatMap(lambda x: x.split())\
-//      .map(lambda x: (x, 1))
-//    bow0.reduceByKey(lambda x,y:x+y).take(50)
+
+
+    //    .filter(x => x.Document)
+    //    .map( x => x.Document.replace(',',' ').replace('.',' ').replace('-',' ').lower())\
+    //      .flatMap(lambda x: x.split())\
+    //      .map(lambda x: (x, 1))
+    //    bow0.reduceByKey(lambda x,y:x+y).take(50)
     //     Delete output directory.
-//    val hadoopConf = new org.apache.hadoop.conf.Configuration
-//    val hdfs = org.apache.hadoop.fs.FileSystem.get(hadoopConf)
-//    try {
-//      hdfs.delete(new org.apache.hadoop.fs.Path(outputDir), true)
-//    } catch {
-//      case _: Throwable => {}
-//    }
+    //    val hadoopConf = new org.apache.hadoop.conf.Configuration
+    //    val hdfs = org.apache.hadoop.fs.FileSystem.get(hadoopConf)
+    //    try {
+    //      hdfs.delete(new org.apache.hadoop.fs.Path(outputDir), true)
+    //    } catch {
+    //      case _: Throwable => {}
+    //    }
 
 
   }
