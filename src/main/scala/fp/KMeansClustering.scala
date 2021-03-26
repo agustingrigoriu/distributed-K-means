58f7262b3316a7051d7ad23e76594e600c56d7cb
Agustin Gregorieu <agustin.gregorieu@gmail.com>
2021-03-26 18:18:47 -0400

    Adding code to recalculate centroids using SparseVectors.

--- src/main/scala/fp/KMeansClustering.scala
+++ src/main/scala/fp/KMeansClustering.scala
@@ -15,9 +15,56 @@ object KMeansClusteringMain {
     val commonIndices = vectorA.indices intersect vectorB.indices
 
     // Calculating the sum of Xi * Yi.
-    val productsSum = commonIndices.map(x => vectorA(x) * vectorB(x)).reduceOption(_+_)
+    val productsSum = commonIndices.map(x => vectorA(x) * vectorB(x)).reduceOption(_ + _)
 
     productsSum.getOrElse(0.0)
+  }
+
+  def recalculateCentroid(clusteredVectors: Iterable[SparseVector]): SparseVector = {
+
+    var avgVector: SparseVector = null
+    val vectorSize: Double = if (clusteredVectors.size == 0) 1 else clusteredVectors.size.toDouble
+
+    clusteredVectors.foreach(vector => {
+
+      if (avgVector == null) {
+        avgVector = vector
+      } else {
+        val newIndices = (avgVector.indices union vector.indices).distinct
+        scala.util.Sorting.quickSort(newIndices)
+        val newValues = newIndices.map(i => {
+
+          val value1 = if (avgVector.indices.contains(i)) avgVector(i) else 0.0
+          val value2 = if (vector.indices.contains(i)) vector(i) else 0.0
+
+          value1 + value2
+        })
+
+        avgVector = new SparseVector(vector.size, newIndices, newValues)
+      }
+
+    })
+
+    val averagedValues = avgVector.values.map(value => value / vectorSize)
+    new SparseVector(avgVector.size, avgVector.indices, averagedValues)
+
+  }
+
+
+  def getClosestCentroid(vector: SparseVector, centroids: Seq[SparseVector]): Long = {
+
+    var minDistance: Double = Double.MaxValue
+    var closestCentroidIdx: Int = -1
+    for (i <- 0 to centroids.length - 1) {
+      val distance = cosineSimilarity(vector, centroids(i))
+
+      if (distance < minDistance) {
+        minDistance = distance
+        closestCentroidIdx = i
+      }
+    }
+
+    closestCentroidIdx
   }
 
   def main(args: Array[String]) {
@@ -86,17 +133,25 @@ object KMeansClusteringMain {
     val normalizedBagOfWords = normalizer.transform(bagOfWords)
 
     // Now we test that the similarity functions are working by self joining the table. Each row should have a 1.0 (approx) score since the are identical.
-    val testRDD: RDD[(Long, SparseVector)] = normalizedBagOfWords
+    val vectors: RDD[(Long, SparseVector)] = normalizedBagOfWords
       .select("index", "normalizedBagOfWords").rdd
       .map(row => (row.getAs[Long](0), row.getAs[SparseVector](1)))
+    // Probably this should be cached.
 
 
-    val testJoin = testRDD.join(testRDD).mapValues(arr => cosineSimilarity(arr._1, arr._2))
+    // Getting K random vectors to use as centroids.
+    val centroids = vectors.takeSample(false, 10).map(tuple => tuple._2).toSeq
 
-    // Printing test example.
-    testJoin.take(40).foreach(println)
 
-    testRDD.saveAsTextFile(outputDir)
+    // Assigning vectors to clusters.
+    val assignedVectors = vectors.map(vector => (getClosestCentroid(vector._2, centroids), vector._2))
+    val newCentroids = assignedVectors.groupByKey().map(groupedVectors => recalculateCentroid(groupedVectors._2))
+
+    newCentroids.saveAsTextFile(outputDir)
+    //    // Printing test example.
+    //    testJoin.take(40).foreach(println)
+    //
+    //    testRDD.saveAsTextFile(outputDir)
 
 
   }
