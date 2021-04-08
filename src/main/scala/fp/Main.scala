a99f4676ed9744c864a3ea1b7ef4f6d4c9d2a4b8
david <djamrog94@gmail.com>
2021-04-08 13:29:18 -0400

    main

--- src/main/scala/fp/Main.scala
+++ src/main/scala/fp/Main.scala
@@ -6,11 +6,10 @@ object Main {
     val prepInputDir: String = args(0)
     val prepOutputDir: String = args(1)
     val kMeansOutDir: String = args(2)
-    val postOutputDir: String = args(3)
-    val tweetsDir: String = args(4)
-    val iters: String = args(5)
-    val K: String = args(6)
-    val version: Int = args(7).toInt
+    val K: String = args(3)
+    val iters: String = args(4)
+    val version: Int = args(5).toInt
+    val postOutputDir: String = args(6)
 
     fp.PreprocessingMain.main(Array(prepInputDir, prepOutputDir))
     if (version == 1) {
@@ -18,6 +17,6 @@ object Main {
     } else {
       fp.KMeansClusteringV2Main.main(Array(prepOutputDir, kMeansOutDir, K, iters))
     }
-    fp.PostProcessingMain.main(Array(kMeansOutDir, postOutputDir, tweetsDir))
+    fp.PostProcessingMain.main(Array(kMeansOutDir, postOutputDir, prepInputDir))
   }
 }
