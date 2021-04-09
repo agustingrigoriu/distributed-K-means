1f86828ae4d3e1fe14c4338d4f8e27e5755ea329
Agustin Gregorieu <agustin.gregorieu@gmail.com>
2021-04-09 15:37:50 -0400

    Updating Main and .gitignore.

--- src/main/scala/fp/Main.scala
+++ src/main/scala/fp/Main.scala
@@ -1,23 +1,30 @@
 package fp
 
+import java.io.File
+
 object Main {
   def main(args: Array[String]): Unit = {
-    //    Here we do pre processing. execution of cluster version, and post processing.
-    val prepInputDir: String = args(0)
-    val prepOutputDir: String = args(1)
-    val kMeansOutDir: String = args(2)
-    val K: String = args(3)
-    val iters: String = args(4)
-    val version: Int = args(5).toInt
-    val postOutputDir: String = args(6)
-    val topKWords: String = args(7)
 
-    fp.PreprocessingMain.main(Array(prepInputDir, prepOutputDir))
+    val inputDir: String = args(0)
+    val outputDir: String = args(1)
+    val preProcessingOutputDir: String = outputDir + File.separator + "preprocessing"
+    val kMeansOutputDir: String = outputDir + File.separator + "k-means"
+    val version: Int = args(2).toInt
+    val K: String = args(3)
+    val I: String = args(4)
+    val topKWords: String = args(5)
+
+    // Pre-Processing Step
+    fp.PreprocessingMain.main(Array(inputDir, preProcessingOutputDir))
+
+    // KMeans Execution
     if (version == 1) {
-      fp.KMeansClusteringMain.main(Array(prepOutputDir, kMeansOutDir, K, iters))
+      fp.KMeansClusteringMain.main(Array(preProcessingOutputDir, kMeansOutputDir, K, I))
     } else {
-      fp.KMeansClusteringV2Main.main(Array(prepOutputDir, kMeansOutDir, K, iters))
+      fp.KMeansClusteringV2Main.main(Array(preProcessingOutputDir, kMeansOutputDir, K, I))
     }
+
+    // Post-Processing Step
     //fp.PostProcessingMain.main(Array(kMeansOutDir, postOutputDir, prepInputDir, topKWords))
   }
 }
